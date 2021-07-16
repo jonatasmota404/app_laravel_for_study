673e98a49dc87fb49d94a3ab72c86e73d5b13c78
Jônatas <jonatasjr.019@gmail.com>
2021-07-16 15:12:55 -0300

    colocando include para incluir o menu em todas as páginas

--- resources/views/site/contact/index.blade.php
+++ resources/views/site/contact/index.blade.php
@@ -3,20 +3,7 @@
 @section('titulo', $titulo)
 
 @section('conteudo')
-    <div class="topo">
-
-        <div class="logo">
-            <img src="{{asset('img/logo.png')}}">
-        </div>
-
-        <div class="menu">
-            <ul>
-                <li><a href="{{ route('site.index') }}">Principal</a></li>
-                <li><a href="{{ route('site.about') }}">Sobre Nós</a></li>
-                <li><a href="{{ route('site.contact') }}">Contato</a></li>
-            </ul>
-        </div>
-    </div>
+    @include('site.layouts._partials.topo')
 
     <div class="conteudo-pagina">
         <div class="titulo-pagina">
